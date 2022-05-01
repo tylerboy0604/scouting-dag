bf49f82c9f8479e140c4b771251504d01bae1aaa
tylerman123 <74601919+tylerboy0604@users.noreply.github.com>
2022-05-01 16:02:18 +0200

    added picture and tried rng

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,38 @@
-const randomItem = ['cent van jaare 60', 'blauwe schroevendraaier', 'konijnen ooren', 'gele boormachine', 'zwembroek', 'schaatsen', 'afstand bestuurbare auto'];
+const randomItem = ['cent van jaren 60', 'blauwe schroevendraaier', 'konijnen oren', 'gele boormachine',
+    'zwembroek', 'schaatsen', 'afstand bestuurbare auto', 'tandenborstel', 'kaasbroodje', 'klok', 'waterpistool',
+    'bad eend', 'mok', 'skylander', 'paars papier', 'pyjama', 'kwast type 13', 'pc fan', 'knuffel', 'hawaii rokje',
+    'spiderman rugtas', 'mickey mouse oren', 'kerstverlichting', 'kerstballen', 'nintendo wii spel', 'roze stropdas',
+    'een dobbelsteen met 20 zijden', 'rieten hoed', 'console controller', 'cactus plantje', 'frikandelbroodje', 'pannenkoekenpan',
+    'donald duck stripboek', 'pokémon kaarten', 'eenhorn onesie'
+
+];
 
 
 
 
 
+
+function rngtext() {
+    let index = Math.floor(Math.random() * 1) + 1;
+    if (index == 1) {
+        document.getElementById('rng-text').innerHTML = 'norvicus bbq komt ook';
+    }
+}
+
+
+
+// window.onload = alert('alert: zorg ervoor dat niemand je ziet');
 
 
 function randomitemcalc() {
     let index = Math.floor(Math.random() * randomItem.length);
     let item = randomItem[index];
-    let naam = randomItem[index]
 
-    document.getElementById('random-item').innerHTML = naam;
+    document.getElementById('random-item').innerHTML = item;
 }
 
 
+
 window.onload = randomitemcalc;
+
+console.log(randomItem.length);
